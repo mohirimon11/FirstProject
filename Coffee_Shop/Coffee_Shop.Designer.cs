1c05e0bdb8351875fada494c2d0fedd405dd4d7f
mohirimon11 <mohirimon11@gmail.com>
2019-09-16 09:14:01 -0700

    Day 3 projrct

--- Coffee_Shop/Coffee_Shop.Designer.cs
+++ Coffee_Shop/Coffee_Shop.Designer.cs
@@ -29,30 +29,33 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.CustomerNameTextBox = new System.Windows.Forms.TextBox();
-            this.contactTextBox = new System.Windows.Forms.TextBox();
-            this.addressTextBox = new System.Windows.Forms.TextBox();
-            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.orderComboBox = new System.Windows.Forms.ComboBox();
             this.saveButton = new System.Windows.Forms.Button();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.contactTextBox = new System.Windows.Forms.TextBox();
+            this.customerNameTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.displayTextBox = new System.Windows.Forms.RichTextBox();
-            this.OrderComboBox = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sizeTextBox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.groupBox1.Controls.Add(this.OrderComboBox);
+            this.groupBox1.Controls.Add(this.orderComboBox);
             this.groupBox1.Controls.Add(this.saveButton);
             this.groupBox1.Controls.Add(this.quantityTextBox);
             this.groupBox1.Controls.Add(this.addressTextBox);
             this.groupBox1.Controls.Add(this.contactTextBox);
-            this.groupBox1.Controls.Add(this.CustomerNameTextBox);
+            this.groupBox1.Controls.Add(this.customerNameTextBox);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
@@ -65,78 +68,18 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Coffee Shop";
             // 
-            // label1
+            // orderComboBox
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Customer Name";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 74);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Contact No";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 107);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Address";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 163);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(33, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Order";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(22, 194);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Quantity";
-            // 
-            // CustomerNameTextBox
-            // 
-            this.CustomerNameTextBox.Location = new System.Drawing.Point(177, 38);
-            this.CustomerNameTextBox.Name = "CustomerNameTextBox";
-            this.CustomerNameTextBox.Size = new System.Drawing.Size(100, 20);
-            this.CustomerNameTextBox.TabIndex = 5;
-            // 
-            // contactTextBox
-            // 
-            this.contactTextBox.Location = new System.Drawing.Point(177, 71);
-            this.contactTextBox.Name = "contactTextBox";
-            this.contactTextBox.Size = new System.Drawing.Size(100, 20);
-            this.contactTextBox.TabIndex = 6;
-            // 
-            // addressTextBox
-            // 
-            this.addressTextBox.Location = new System.Drawing.Point(177, 100);
-            this.addressTextBox.Name = "addressTextBox";
-            this.addressTextBox.Size = new System.Drawing.Size(100, 20);
-            this.addressTextBox.TabIndex = 7;
-            // 
-            // quantityTextBox
-            // 
-            this.quantityTextBox.Location = new System.Drawing.Point(177, 191);
-            this.quantityTextBox.Name = "quantityTextBox";
-            this.quantityTextBox.Size = new System.Drawing.Size(100, 20);
-            this.quantityTextBox.TabIndex = 9;
+            this.orderComboBox.FormattingEnabled = true;
+            this.orderComboBox.Items.AddRange(new object[] {
+            "Black Coffee - 120",
+            "Cold Coffee  - 100 ",
+            "Hot Coffee - 90",
+            "Reguler coffee - 80"});
+            this.orderComboBox.Location = new System.Drawing.Point(177, 154);
+            this.orderComboBox.Name = "orderComboBox";
+            this.orderComboBox.Size = new System.Drawing.Size(100, 21);
+            this.orderComboBox.TabIndex = 8;
             // 
             // saveButton
             // 
@@ -148,39 +91,130 @@
             this.saveButton.UseVisualStyleBackColor = true;
             this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(177, 191);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(100, 20);
+            this.quantityTextBox.TabIndex = 9;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(177, 100);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(100, 20);
+            this.addressTextBox.TabIndex = 7;
+            // 
+            // contactTextBox
+            // 
+            this.contactTextBox.Location = new System.Drawing.Point(177, 71);
+            this.contactTextBox.Name = "contactTextBox";
+            this.contactTextBox.Size = new System.Drawing.Size(100, 20);
+            this.contactTextBox.TabIndex = 6;
+            // 
+            // customerNameTextBox
+            // 
+            this.customerNameTextBox.Location = new System.Drawing.Point(177, 38);
+            this.customerNameTextBox.Name = "customerNameTextBox";
+            this.customerNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.customerNameTextBox.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Quantity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Order";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contact No";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name";
+            // 
             // displayTextBox
             // 
-            this.displayTextBox.Location = new System.Drawing.Point(499, 28);
+            this.displayTextBox.Location = new System.Drawing.Point(499, 128);
             this.displayTextBox.Name = "displayTextBox";
-            this.displayTextBox.Size = new System.Drawing.Size(251, 291);
+            this.displayTextBox.Size = new System.Drawing.Size(251, 191);
             this.displayTextBox.TabIndex = 1;
             this.displayTextBox.Text = "";
             // 
-            // OrderComboBox
+            // label6
             // 
-            this.OrderComboBox.FormattingEnabled = true;
-            this.OrderComboBox.Items.AddRange(new object[] {
-            "Black Coffee - 120",
-            "Cold Coffee  - 100 ",
-            "Hot Coffee - 90",
-            "Reguler coffee - 80"});
-            this.OrderComboBox.Location = new System.Drawing.Point(177, 154);
-            this.OrderComboBox.Name = "OrderComboBox";
-            this.OrderComboBox.Size = new System.Drawing.Size(100, 21);
-            this.OrderComboBox.TabIndex = 8;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(499, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(210, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "How many customer information you need?";
             // 
-            // Form1
+            // sizeTextBox
+            // 
+            this.sizeTextBox.Location = new System.Drawing.Point(608, 44);
+            this.sizeTextBox.Name = "sizeTextBox";
+            this.sizeTextBox.Size = new System.Drawing.Size(101, 20);
+            this.sizeTextBox.TabIndex = 3;
+            // 
+            // showButton
+            // 
+            this.showButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.showButton.Location = new System.Drawing.Point(634, 70);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 4;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = false;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // Coffee_Shop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1053, 528);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.sizeTextBox);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.displayTextBox);
             this.Controls.Add(this.groupBox1);
-            this.Name = "Form1";
+            this.Name = "Coffee_Shop";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -191,14 +225,17 @@
         private System.Windows.Forms.TextBox quantityTextBox;
         private System.Windows.Forms.TextBox addressTextBox;
         private System.Windows.Forms.TextBox contactTextBox;
-        private System.Windows.Forms.TextBox CustomerNameTextBox;
+        private System.Windows.Forms.TextBox customerNameTextBox;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.RichTextBox displayTextBox;
-        private System.Windows.Forms.ComboBox OrderComboBox;
+        private System.Windows.Forms.ComboBox orderComboBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox sizeTextBox;
+        private System.Windows.Forms.Button showButton;
     }
 }
 
--- Coffee_Shop/Coffee_Shop.cs
+++ Coffee_Shop/Coffee_Shop.cs
@@ -12,6 +12,19 @@ namespace Coffee_Shop
 {
     public partial class Coffee_Shop : Form
     {
+        string[] customerArray = new string[20];
+        string[] addressArray = new string[20];
+        string[] orderArray = new string[20];
+        int[] contactArray = new int[20];
+        int[] quantityArray = new int[20];
+        int[] priceArray = new int[20];
+        
+
+        string Customer, Address, Order;
+        int Contact, Quantity, price, index=0;
+
+      
+
         public Coffee_Shop()
         {
             InitializeComponent();
@@ -19,25 +32,76 @@ namespace Coffee_Shop
 
         private void saveButton_Click(object sender, EventArgs e)
         {
-            string Customer, Address, Order;
-            int Contact, Quantity;
-            if (contactTextBox.Text == "" || CustomerNameTextBox.Text == "" || addressTextBox.Text == "" || OrderComboBox.Text == "" || quantityTextBox.Text == "")
+            
+            if (contactTextBox.Text == "" || customerNameTextBox.Text == "" || addressTextBox.Text == "" || orderComboBox.Text == "" || quantityTextBox.Text == "")
             {
                 MessageBox.Show("Enter value please");
             }
-            else
+            else 
             {
-                Customer = CustomerNameTextBox.Text;
+
+                Customer = customerNameTextBox.Text;
                 Contact = Convert.ToInt32(contactTextBox.Text);
                 Address = addressTextBox.Text;
-                Order = OrderComboBox.Text;
+                Order = orderComboBox.Text;
                 Quantity = Convert.ToInt32(quantityTextBox.Text);
-               // string Result = Order + Quantity;
-                MessageBox.Show(" Customar Name: " + Customer + " \n Contact No: " + Contact + "\n Address: " + Address +
-                    "\n\n Order: " + Order + "\n Quantity: " + Quantity);
-                displayTextBox.Text = "Custmar Purchase Information:\n\n Customar Name: " + Customer + " \n Contact No: " + Contact + "\n Address: " + Address +
-                    "\n\n Order: " + Order + "\n Quantity: " + Quantity + "\n________________________\n Total Price is: ";
+                string Result = Order + Quantity;
+                string message = " Customar Name: " + Customer + " \n Contact No: " + Contact + "\n Address: " + Address +
+                        "\n\n Order: " + Order + "\n Quantity: " + Quantity;
+               
+                if (orderComboBox.Text== "Black Coffee - 120")
+                {
+                    price = Convert.ToInt32(quantityTextBox.Text) * 120;
+                    MessageBox.Show(message);
+                    displayTextBox.Text = "Custmar Purchase Information:\n\n" + message + "\n________________________\n Total Price is: " +price;
+                }
+                if(orderComboBox.Text == "Cold Coffee  - 100")
+                {
+                    price = Convert.ToInt32(quantityTextBox.Text) * 100;
+                    MessageBox.Show(message);
+                    displayTextBox.Text = "Custmar Purchase Information:\n\n" + message + "\n________________________\n Total Price is: " + price;
+
+                }
+                if (orderComboBox.Text == "Hot Coffee - 90")
+                {
+                    price = Convert.ToInt32(quantityTextBox.Text) * 90;
+                    MessageBox.Show(message);
+                    displayTextBox.Text = "Custmar Purchase Information:\n\n"+ message + "\n________________________\n Total Price is: " + price;
+
+                }
+                if (orderComboBox.Text == "Reguler coffee - 80")
+                {
+                    price = Convert.ToInt32(quantityTextBox.Text) * 80;
+                    MessageBox.Show(message);
+                    displayTextBox.Text = "Custmar Purchase Information:\n\n"+ message + "\n________________________\n Total Price is: " + price;
+
+                }
+                customerArray[index] = customerNameTextBox.Text;
+                contactArray[index] = Convert.ToInt32(contactTextBox.Text);
+                addressArray[index] = addressTextBox.Text;
+                orderArray[index] = orderComboBox.Text;
+                quantityArray[index] = Convert.ToInt32(quantityTextBox.Text);
+                priceArray[index] = price;
+                index++;
+                
+
+
             }
+        }
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            string Show_Result = "";
+            int Size = Convert.ToInt32(sizeTextBox.Text);
+            for(index=0;index<Size;index++)
+                //string message = " Customar Name: " + Customer + " \n Contact No: " + Contact + "\n Address: " + Address +
+                //        "\n\n Order: " + Order + "\n Quantity: " + Quantity;
+            {
+                Show_Result += " \n\n\n"+ " Customar Name: " + customerArray[index] + " \n Contact No: " + contactArray[index]
+                    + "\n Address: " + addressArray[index]+ "\n\n Order: " + orderArray[index]+ "\n Quantity: " +
+                    quantityArray[index]+ "\n________________________\n Total Price is: " + priceArray[index];
+            }
+            displayTextBox.Text = Show_Result;
+
         }
     }
 }
